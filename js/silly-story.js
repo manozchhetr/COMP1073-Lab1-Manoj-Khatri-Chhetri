a916a85f63d8fd4724e18489b4cf42bb3b131444
manozchhetri <156447784+manozchhetri@users.noreply.github.com>
2024-09-24 23:34:10 -0400

    third

--- js/silly-story.js
+++ js/silly-story.js
@@ -18,21 +18,22 @@ var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. Whe
 var insertX = ['Donald Trump', 'Jackie Chan', 'Santa Claus'];
 var insertY = ['Area 51', 'Death Valley', 'Aruba'];
 var insertZ = ['spontaneously combusted', 'rapidly sublimated', 'evaporated instantly'];
-
 // FUNCTIONS
 
-/* STEP 2: have a look at the following function - if you call this function and pass it an array, it will return one of the elements of that array randomly */
-function randomValueFromArray(array){
-	return array[Math.floor(Math.random()*array.length)];
+// STEP 2: Function to return a random value from an array
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
 }
 
-/* STEP 6: Review the partially complete result() function below */
+// STEP 6: Partially complete result() function
 function result() {
-	// STEP 7: Create a new variable called newStory and set it to the value of storyText - we don't want to overwrite the original story!
+    // STEP 7: Create a newStory variable that is a copy of storyText
+    let newStory = storyText;
 
-	/* STEP 8: Use the randomValueFromArray() function to generate a value for each of three new variables - xItem, yItem, and zItem
-	Call up the function and for each variable, pass it the array from which to grab a random string - for example if insertW was an array of strings, I would type:
-	var wItem = randomValueFromArray(insertW);
+    // STEP 8: Generate random values from the arrays
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
 
 	/* STEP 9: Replace the three placeholders in the newStory string — :insertx:, :inserty:, and :insertz: — with the strings stored in xItem, yItem, and zItem. Each time, be sure to update the variable newStory (with +=). You might need to do one of the above replacements twice! */
 
